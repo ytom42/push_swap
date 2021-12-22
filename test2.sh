8a3fe3a180b3ffdb24bc00d7b0b3da3c29baec0c
yutaro <ytom42tk@gmail.com>
2021-12-22 18:05:45 +0900

    update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,5 +1,9 @@
-for i in $(seq 1 10000); do {
+for i in $(seq 1 1000); do {
 	echo -n $i":"
-	ARG=$(jot -r -s " " 100 -2147483648 2147483647);./push_swap $ARG | ./checker_Mac $ARG;
+	ARG=$(jot -r -s " " 100 -2147483648 2147483647);./push_swap $ARG | ./checker_Mac $ARG; line=$(./push_swap $ARG | wc -l);
+	if [ $line -ge 700 ]; then
+		echo "==============================================="$line
+		# exit 1
+	fi
 }
 done
